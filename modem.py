514dddfb0b191df978280956e81418651e1149d1
niiccoo2 <nicosmith873@gmail.com>
2025-05-26 09:47:17 -0400

    kinda got scrolling working

--- modem.py
+++ modem.py
@@ -6,7 +6,7 @@ Demo: Send Simple SMS Demo
 Simple demo to send sms via gsmmodem package
 """
 
-from gsmmodem.modem import GsmModem, SentSms  # type: ignore
+from gsmmodem.modem import GsmModem, SentSms, CmsError  # type: ignore
 import sys
 import os
 libdir = "./waveshare_epd"
@@ -36,6 +36,31 @@ ScreenImage1 = Image.new('1', (epd.height, epd.width), 255)  # 255: Set all pixe
 draw = ImageDraw.Draw(ScreenImage1)
 
 modem = None  # Global variable to hold the modem instance
+
+def nothing():
+    pass
+
+state = {
+    'screen': nothing,
+    'page': 0,
+    'index': 0,
+    'number': '+10000000000'
+}
+
+original_handle_status_report = GsmModem._handleSmsStatusReport
+
+# Define a patched version
+def patched_handle_status_report(self, line):
+    try:
+        original_handle_status_report(self, line)
+    except CmsError as e:
+        if e.code == 321:
+            print("Ignored CMS ERROR 321 (no delivery report found)")
+        else:
+            raise  # Reraise other errors
+
+# Patch the method
+GsmModem._handleSmsStatusReport = patched_handle_status_report
 
 def extract_conversation_details(filepath):
     data = read_json_file(filepath)
@@ -82,6 +107,10 @@ conversation_keys = list(conversation_details.keys())
 def process_incoming_sms(sms): # Function that runs whenever a sms is received
     print(f"Got text from {sms.number}: {sms.text}")
     store.save_message(sms.number, sms.text)
+    if state['screen'] == open_conversation:
+        state['screen'](state['number'])
+    else:
+        state['screen']()
 
 def init_modem(): # Initalizes the modem
     global modem_init
@@ -128,24 +157,82 @@ def draw_messages_screen(selected_index, current_page):
             draw_conversation(index * 100, details['name'], convert_time(details['last_message_time']), details['last_message'])
     epd.display_Partial(epd.getbuffer(ScreenImage1))
 
-def open_conversation(number):
+def draw_message_view(number, current_page):
     clear_draw(draw)
+
     conversation = load_conversation_data(number)
-    print(f"Contact Name: {conversation['contact_name']}")
-    for message in conversation.get('messages', []):
-        print(f"Message ID: {message.get('id', 'N/A')}")
-        print(f"Content: {message.get('content', 'N/A')}")
-        print(f"Timestamp: {message.get('timestamp', 'N/A')}")
-        print(f"Is Incoming: {message.get('is_incoming', 'N/A')}")
-        print(f"Status: {message.get('status', 'N/A')}")
-        print("-" * 40)  # Separator for readability
+    all_messages = conversation.get('messages', [])[::-1]  # Reverse the list to start with the most recent messages
+
+    page_stop = 0
+    adding_y = 400  # Start drawing from the bottom of the screen
+    messages_per_page = []
+    current_page_messages = []
+
+    for message in all_messages:
+        # Calculate the height of the message without drawing it
+        height = draw_with_wrap(draw, (10, adding_y), message.get('content', 'N/A'), 200, font=font(15), alignment='left', no_draw=True)
+        
+        # Check if the message fits on the current page
+        if adding_y - height < page_stop:
+            # If it doesn't fit, store the current page's messages and start a new page
+            messages_per_page.append(current_page_messages)
+            current_page_messages = []
+            adding_y = 380  # Reset y position for the new page
+
+        # Add the message to the current page
+        current_page_messages.append(message)
+        adding_y -= height + 10
+
+    # Add the last page's messages
+    if current_page_messages:
+        messages_per_page.append(current_page_messages)
+
+    # Display the messages for the current page
+    adding_y = 380  # Reset y position for drawing
+    for message in messages_per_page[current_page]:
+        if message.get('is_incoming', 'N/A') == True:
+            draw_with_wrap(draw, (10, adding_y), message.get('content', 'N/A'), 200, font=font(15))
+        elif message.get('is_incoming', 'N/A') == False:
+            draw_with_wrap(draw, (10, adding_y), message.get('content', 'N/A'), 200, font=font(15), alignment='right')
+        adding_y -= height + 10
+
+    epd.display_Partial(epd.getbuffer(ScreenImage1))
+
+
+
+
+def open_conversation(number):
+    global state
+    state['number'] = number
+    state['screen'] = open_conversation
+    page_number = 0
+    draw_message_view(number, page_number)
+    
+    while True:
+        user_input = input("Message: ")
+        if user_input == 'exit':
+            return
+        elif user_input == 'w':
+            page_number += 1
+            if page_number < 0:
+                page_number = 0
+        elif user_input == 's':
+            page_number -= 1
+        else:
+            send_sms_message(number, user_input)
+            store.save_message(number, user_input, is_incoming=False)
+        clear_draw(draw)
+        draw_message_view(number, page_number)
+
 
 def messages_app(): 
     global conversation_details
+    global state
+    state['screen'] = messages_app
     conversation_details = extract_conversation_details("data/conversations.json")
     conversation_keys = list(conversation_details.keys())
-    selected_index = 0
-    current_page = 0
+    # selected_index = 0
+    # current_page = 0
     conversations_per_page = 4
     total_pages = (len(conversation_keys) + conversations_per_page - 1) // conversations_per_page
 
@@ -155,29 +242,29 @@ def messages_app():
     if modem_init == False:
         init_modem()
 
-    draw_messages_screen(selected_index, current_page)
+    draw_messages_screen(state['index'], state['page'])
 
     while True:
         user_input = input("Input: ")
         if user_input == "exit":
-            break
+            return
         elif user_input == "s":
-            selected_index += 1
-            if selected_index + (current_page) * conversations_per_page >= len(conversation_keys):
-                selected_index = (len(conversation_keys) % conversations_per_page) - 1
-            if selected_index >= conversations_per_page:
-                selected_index = 0
-                current_page += 1
-                if current_page >= total_pages:
-                    current_page = total_pages - 1
+            state['index'] += 1
+            if state['index'] + (state['page']) * conversations_per_page >= len(conversation_keys):
+                state['index'] = (len(conversation_keys) % conversations_per_page) - 1
+            if state['index'] >= conversations_per_page:
+                state['index'] = 0
+                state['page'] += 1
+                if state['page'] >= total_pages:
+                    state['page'] = total_pages - 1
         elif user_input == "w":
-            selected_index -= 1
-            if selected_index < 0:
-                selected_index = conversations_per_page - 1
-                current_page -= 1
-                if current_page < 0:
-                    current_page = 0
-                    selected_index = 0
+            state['index'] -= 1
+            if state['index'] < 0:
+                state['index'] = conversations_per_page - 1
+                state['page'] -= 1
+                if state['page'] < 0:
+                    state['page'] = 0
+                    state['index'] = 0
         
         ### Logic that skips pages ###
         # elif user_input == "n":  # Next page
@@ -192,13 +279,13 @@ def messages_app():
 
         elif user_input == "e":
             print("Running the selected app")
-            open_conversation(conversation_keys[selected_index + current_page * conversations_per_page])
+            open_conversation(conversation_keys[state['index'] + state['page'] * conversations_per_page])
             clear_screen()
-        print(selected_index)
-        print(current_page)
+        print(state['index'])
+        print(state['page'])
         print(conversation_keys)
         clear_draw(draw)
-        draw_messages_screen(selected_index, current_page)
+        draw_messages_screen(state['index'], state['page'])
 
 
     #time.sleep(10)
@@ -216,12 +303,12 @@ def messages_app():
 
 
 if __name__ == "__main__":
-    # open_conversation("+16172060139")
     epd.init()
     epd.Clear()
-    text = "This is a lot of text, but we need a lot of, text so that we can test, if this thing wraps good."
-    draw_with_wrap(draw, (10, 10), text, 200, font=font(15), alignment='right')
+    print(draw_with_wrap(draw, (10, 10), "This", 200, font(15), alignment='left'))
+    print(draw_with_wrap(draw, (10, 40), "This should be a lot of lines that we need to test this thing.", 200, font(15), alignment='left'))
+    draw.line([(10,50), (10, 62)], fill=None, width=2, joint=None)
+    draw.line([(10,70), (10, 87)], fill=None, width=2, joint=None)
     epd.display_Partial(epd.getbuffer(ScreenImage1))
     epd.sleep()
     exit()
-
